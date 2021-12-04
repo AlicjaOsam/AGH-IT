d868b65195f1a6201722f64e9e37dfa5eed67f1e
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2021-12-04 23:43:48 +0100

    Add files via upload

---
+++ lab6/lab6_cw3_c.js
@@ -0,0 +1,3 @@
+context.font = "bold 12px sans-serif";
+context.fillText("x", 248, 43);
+context.fillText("y", 58, 165);
---
+++ lab6/lab6_cw3_d.js
@@ -0,0 +1,10 @@
+startx = 600;
+starty = 400;
+endx = 0;
+endy = 0;
+  
+context.beginPath();
+context.moveTo(startx, starty);
+context.lineTo(endx, endy);
+context.strokeStyle = "black";
+context.stroke();
